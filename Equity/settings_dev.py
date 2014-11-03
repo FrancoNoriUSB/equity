17654d6377b17e5e261754548c9f12d92b00e065
Franco Nori <francong2@gmail.com>
2014-11-02 21:05:13 -0430

    evolution removed

--- Equity/settings_dev.py
+++ Equity/settings_dev.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_evolution',
     'django_countries',
     'inmuebles',
     'noticias'
