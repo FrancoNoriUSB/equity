6d82462642f122199f8ec4ea7676d273811f8679
Franco Nori <francong2@gmail.com>
2014-12-19 01:53:36 -0430

    migrations

---
+++ inmuebles/migrations/0007_modulo_plano.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inmuebles', '0006_auto_20141217_1511'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='modulo',
+            name='plano',
+            field=models.ImageField(default='uploads/planos/plano.png', upload_to=b'uploads/planos/'),
+            preserve_default=False,
+        ),
+    ]
