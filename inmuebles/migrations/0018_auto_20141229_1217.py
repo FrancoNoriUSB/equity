7892d3be478fdd073cff3f2bb2192f755bfa2fcf
Franco Nori <francong2@gmail.com>
2014-12-29 16:24:35 -0430

    Ajustes en inmuebles

---
+++ inmuebles/migrations/0018_auto_20141229_1217.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inmuebles', '0017_banner_url'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='modulo',
+            name='precio',
+            field=models.DecimalField(max_digits=25, decimal_places=5),
+            preserve_default=True,
+        ),
+    ]
