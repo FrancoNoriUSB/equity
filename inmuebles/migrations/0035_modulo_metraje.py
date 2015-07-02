893c36c88cda50a1e091c6019d8abbc7d8649c62
Franco Nori <francong2@gmail.com>
2015-07-02 13:18:50 -0430

    Arreglos en busqueda y filtrado

--- inmuebles/migrations/0035_modulo_metraje.py
+++ inmuebles/migrations/0035_modulo_metraje.py
@@ -6,7 +6,7 @@ from django.db import models, migrations
 def create_modulo_metraje(apps, schema_editor):
     # We can't import the Category model directly as it may be a newer
     # version than this migration expects. We use the historical version.
-    Modulo = apps.get_model("modulos", "Modulo")
+    Modulo = apps.get_model("inmuebles", "Modulo")
     for modulo in Modulo.objects.all():
         modulo.slug = int(modulo.metros)
         modulo.save()
